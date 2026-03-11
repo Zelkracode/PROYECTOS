7d2af113de5bace19570519f82f05ee425dab15d
Zelkra <lush.matdip@gmail.com>
2026-03-11 12:10:03 -0600

    Corrijo carpeta python-cybersecurity

---
+++ python-cybersecurity/Proyect_01.py
@@ -0,0 +1,22 @@
+# Primer scaner 
+
+import socket
+
+host = input("Ingrese la IP o dominio: ")
+
+print(f"\nEscaneando {host}\n")
+
+for puerto in range(1,1025):
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    s.settimeout(1)
+
+    resultado = s.connect_ex((host, puerto))
+
+    if resultado == 0:
+        print(f"Puerto {puerto} ABIERTO")
+
+    s.close()
+    
+    
